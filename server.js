29643cf732b971d21d41f860f5342fc83957508c
Pavle <milosavljevic2007pavle1730@gmail.com>
2026-04-17 14:12:28 +0200

    Initialize commit

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const { get } = require("node:http");
-
-const express = required("express");
+const express = require("express");
 const app = express();
 
-app.get("/tasks", (req,ges) => {
-    res.send("Tasks side")
-})
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from my express app! You guys are amazing!!! ❤️");
+});
+
+app.get("/about", (req, res) => {
+  console.log("Haha");
+
+  res.send("This is the about page for a really aawesome teacher!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
