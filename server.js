bfe8e4ac98efad602452e0b9e7f4f72d2e61a6db
Pavle <milosavljevic2007pavle1730@gmail.com>
2026-04-20 12:31:49 +0200

    Har gjort branchen tasks och gjort tasken för Data delete

--- server.js
+++ server.js
@@ -9,30 +9,35 @@ let tasks = [
   { id:3, namn: "Ingela", År: 41 }
 ];
 
+
+
 app.get("/", (req, res) => {
   res.send("Hello from my express app! You guys are amazing!!! ❤️");
 });
 
 app.get("/about", (req, res) => {
   console.log("HejHej");
+
   res.send("This is the about page for a really aawesome teacher!!!");
 });
 
-// ---Tasks finns här---
-app.delete("/tasksuppgifter/:id", (req, res) => { //Lektion Delete Data
-  const id = Number(req.params.id)
-
-  const tasksIndex = tasks.findIndex( t => t.id === id);
-
-  if (tasksIndex === -1 || tasksIndex > 3){
+app.delete("/tasks/:id", (req, res) => {
+  const id = req.params.id;
+ 
+  const taskIndex = tasks.findIndex(
+    t => t.id == id
+  );
+ 
+  if (taskIndex === -1){
     return res.status(404).send("Task not found")
   }
-  tasks.splice(tasksIndex,1);
+
+
+  tasks.splice(taskIndex, 1);
   res.status(200).send("Task succesfully deleted")
+})
 
-}) // Slutet Letkion Delete Data
 
-// ---Slutet av Tasks---
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
